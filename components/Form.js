5a91103d48fce769deae78938f9e3f24fa79aa62
Android Studio <devandroid@ort>
2025-05-26 08:59:33 -0300

    Formulario (sin estilos) hecho

---
+++ components/Form.js
@@ -0,0 +1,13 @@
+import { View } from 'react-native'
+import React from 'react'
+import Inputs from './Inputs.js'
+import Buttons from './Buttons.js'
+
+export default function Form() {
+    return (
+        <View>
+            <Inputs />
+            <Buttons />
+        </View>
+    )
+}
